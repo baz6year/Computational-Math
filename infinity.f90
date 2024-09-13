94eda88530af70a1c91f2b5aace5b3c7c28fa931
alimanbazarbekova <156849572+baz6year@users.noreply.github.com>
2024-09-13 16:25:10 +0600

    task7

---
+++ infinity.f90
@@ -0,0 +1,12 @@
+program infinity
+    implicit none
+    integer:: n
+    real:: I 
+    n = 0
+    I = 1
+    do while(2 * I > I)
+        n = n + 1
+        I = 10 * I 
+    end do
+    print *, n, I 
+end program infinity
