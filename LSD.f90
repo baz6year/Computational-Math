6d0b2f5cdc2c4160d7a2f105d097ca52d1411f37
alimanbazarbekova <156849572+baz6year@users.noreply.github.com>
2024-09-29 17:15:46 +0600

    lsd

--- LSD.f90
+++ LSD.f90
@@ -1,21 +1,22 @@
-program UpperSum
-integer:: n, a, b, height
-real:: width, sum, area, x
-n = 0
+program LowerSum
+integer:: n, z
+real:: width, sum, area, x, a, b, height
+read (*,*) n
+z = 0
 x = 0.0
-a = 0
-b = 3
+a = 4.d0 * datan(1.d0)
+b = a*1.5
 sum = 0.0
-width = real(b) / 10.0
-do while(n < 10)
-height = 2 ** x - 1
+width = real(b) / n
+do while(z < n)
+height = sin(x)
 area = height * width
-x = x + width
 sum = sum + area
-n=n+1
+x = x + width
+z=z+1
 end do 
 print *, sum
-end program UpperSum
+end program LowerSum
 
 
 
